63b2e1b7e63ac20af55b70674d9952ef33b81463
Joanne Taylor deKay <jdekay1@jhu.edu>
2018-08-28 13:33:52 -0400

    Day 2 Question 2

---
+++ day2-lunch/day2-exercise-2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    f = open( sys.argv[1] )
+else: 
+    f = sys.stdin
+ 
+count =0    
+for i, line in enumerate( f ):
+    if line[0] == "@":
+        continue
+    if "NM:i:0" in line:
+        count += 1
+
+
+print(count)
+  #      if count >10:
+  #          break    
+        
+#    fields = line.rstrip("\r\n").split("\t")
+ #   tx_len = int(fields[4]) - int(fields[3])
+#    if tx_len > 10000:
+#        print( fields[5], tx_len )
