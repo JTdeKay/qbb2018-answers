dc03f1431efd8f80ff13874c2d12565340e1b511
Joanne Taylor deKay <jdekay1@jhu.edu>
2018-08-29 14:11:05 -0400

    Day 3 Question 3 prot coding

---
+++ day3-lunch/neargene.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+
+f = open( sys.argv[1])
+
+find_pos = 21378950
+short_dis = 10**9
+close_gene = "NOPE"
+
+for line in f :
+    if line.startswith("#"):
+        continue
+    fields = line.rstrip("\r\n").split()
+    if fields[0] == "3R" and fields[2] == "gene" and fields[17] == "\"protein_coding\";":
+        my_dist = 0
+        if find_pos < int(fields[3]):
+            my_dist = int(fields[3]) - find_pos
+        elif find_pos > int(fields[4]):
+            my_dist = find_pos - int(fields[4])
+        if short_dis > my_dist:
+            short_dis = my_dist    
+            close_gene = fields[13]
+
+print(close_gene, short_dis)
+            
+
+    # if fields[0] == "3R" and fields[2] == "gene" and fields[17] != "\"protein_coding\";":
+    #     my_dist = 0
+    #     if find_pos < fields[3]:
+    #         my_dist = fields[3] - find_pos
+    #     elif find_pos > fields[4]:
+    #         my_dist = find_pos - fields[4]
