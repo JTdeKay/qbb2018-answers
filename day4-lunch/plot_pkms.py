c3950d5db336901fc9ce416632dd4d76844b3867
Joanne Taylor deKay <jdekay1@jhu.edu>
2018-09-03 14:40:24 -0400

    Day 4 lunch Q2 with modifications and notations

--- day4-lunch/plot_pkms.py
+++ day4-lunch/plot_pkms.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Usage: ./merge_fpkms.sh <threshold> <ctab_file1> <ctab_file2> ... <ctab_filen>
+Usage: ./plot_pkms.py <threshold> <ctab_file1> <ctab_file2> ... <ctab_filen>
 """
 
 import sys
@@ -14,17 +14,17 @@ t = sys.argv[1]
 
 name1 = sys.argv[1].split( os.sep )[-2]
 fpkm1 = pd.read_csv( sys.argv[1], sep="\t", index_col="t_name" ).loc[:,"FPKM"]
-#logfpkm1 = np.log(fpkm1+1)
 
 name2 = sys.argv[2].split( os.sep )[-2]
 fpkm2 = pd.read_csv( sys.argv[2], sep="\t", index_col="t_name" ).loc[:,"FPKM"]
 
 
-
+#fitting curve
 coeff = np.polyfit(fpkm1,fpkm2,1) 
 polynomial = np.poly1d(coeff)
 x = np.linspace(0, 10000)
 
+#Plot parameters: label title, axes, log transform, adj transparency
 
 fig, ax= plt.subplots()
 ax.scatter(fpkm1, fpkm2, alpha = 0.2, s = 3)
