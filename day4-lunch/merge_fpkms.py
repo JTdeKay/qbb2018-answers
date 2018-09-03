37469e22293f032ed8179cf4d42f988576d5d151
Joanne Taylor deKay <jdekay1@jhu.edu>
2018-09-03 14:45:24 -0400

    Day 4 Q1 py file with modifications and notations

--- day4-lunch/merge_fpkms.py
+++ day4-lunch/merge_fpkms.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Usage: ./merge_fpkms.sh <threshold> <ctab_file1> <ctab_file2> ... <ctab_filen>
+Usage: ./merge_fpkms.py <threshold> <ctab_file1> <ctab_file2> ... <ctab_filen>
+
+Combine FPKM values from 2 or more ctab files specifying a threshold for total FPKM.
 """
 
 import sys
@@ -19,12 +21,12 @@ for transcript in range( 2,len( sys.argv )):
     fpkm = pd.read_csv( sys.argv[transcript], sep="\t", index_col="t_name" ).loc[:,"FPKM"]
     fpkms[name] = fpkm
 
-
+#set dataframe
 fpkms_df = pd.DataFrame( fpkms )
 
 fpkms_df["Sum"] = fpkms_df[list(fpkms_df)].sum(axis=1)
 
-
+#define row of interest
 roi = fpkms_df.loc[:,"Sum"] > float(t)
 fpkms_df.loc[roi, fpkms_df.columns != "Sum"].to_csv(sys.stdout, sep="\t") 
 
