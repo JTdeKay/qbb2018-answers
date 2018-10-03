7dbfbb3c05f02e3f8e5697773b9c828f5cad1e7c
Joanne Taylor deKay <jdekay1@jhu.edu>
2018-10-03 17:10:20 -0400

    Notes for file prep

---
+++ week4/wk4notes.py
@@ -0,0 +1,12 @@
+"""Plink and GWAS with provided BYxRM_segs_saccer3.bam.simplified.vcf file and BYxRM_PhenoData.txt"""
+
+# install plink 
+# conda install plink
+
+# download genotype and phenotype files
+# gunzip <genotype file>
+
+# plink PCA
+# plink BYxRM_segs_saccer3.bam.simplified.vcf --pca --allow-extra-chr --mind
+
+# yields eigenvec and eigenval files
